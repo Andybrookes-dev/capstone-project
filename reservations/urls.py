0678b6dcba7b8cd8af9345d9e418cb8f11325f34
AndyMusicproducer <andybrookes78@icloud.com>
2025-11-26 12:16:18 +0000

    added base.html and reservstion_view.html

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,15 @@
+# from django.urls import path
+# from . views import reservation_view
+
+# urlpatterns = [
+#     path('', reservation_view, name='reservation'),
+# ]
+
 from django.urls import path
-from . views import reservation_view
+from .views import reservation_view, book_reservation, reservation_success
 
 urlpatterns = [
-    path('', reservation_view, name='reservation'),
+    path("", reservation_view, name="reservation"),
+    path("book/", book_reservation, name="book_reservation"),
+    path("success/", reservation_success, name="reservation_success"),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib import messages
+from .forms import ReservationForm
+
 def reservation_view(request):
-    return HttpResponse("<h1>This is the reservation view.</h1>")
+    return render(request, "reservations/reservation_view.html")
+
+def book_reservation(request):
+    if request.method == "POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            if request.user.is_authenticated:
+                reservation.user = request.user
+            reservation.save()
+            messages.success(request, "Your reservation has been booked successfully!")
+            return redirect("reservation_success")  # redirect to success page
+        else:
+            messages.error(request, "There was an error with your booking. Please try again.")
+    else:
+        form = ReservationForm()
+
+    return render(request, "reservations/book_reservation.html", {"form": form})
+
+def reservation_success(request):
+    return render(request, "reservations/reservation_success.html")
